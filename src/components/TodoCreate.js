8bf99de9d4219c8b5c8bbf88681abad30ac50b45
jy <jaey5oung@naver.com>
2021-01-31 19:35:23 +0900

    styled-components

---
+++ src/components/TodoCreate.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { MdAdd } from 'react-icons/md';
+
+const CircleButton = styled.button`
+  background: #38d9a9;
+  &:hover {
+    background: #63e6be;
+  }
+  &:active {
+    background: #20c997;
+  }
+
+  z-index: 5;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  display: block;
+  align-items: center;
+  justify-content: center;
+  font-size: 60px;
+  position: absolute;
+  left: 50%;
+  bottom: 0px;
+  transform: translate(-50%, 50%);
+  color: white;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+function TodoCreate() {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => setOpen(!open);
+  return (
+    <CircleButton open={open}>
+      <MdAdd />
+    </CircleButton>
+  );
+}
+
+export default TodoCreate;
